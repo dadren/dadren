268980c0080934ed4008620e6c71db9c5eb72620
asdf <tehasdf@gmail.com>
2016-02-26 16:36:18 +0100

    texture loader examples - build.sh

--- examples/texture_load/build.sh
+++ examples/texture_load/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 mkdir -p bin
+nim c $@ --nimcache=$(pwd)/.nimcache --debugger:native --out:bin/texture_load -p=$(pwd)/../../ texture_load.nim
+nim c $@ --nimcache=$(pwd)/.nimcache --debugger:native --out:bin/atlas_load -p=$(pwd)/../../ atlas_load.nim
 nim c $@ --nimcache=$(pwd)/.nimcache --debugger:native --out:bin/namedatlas_load -p=$(pwd)/../../ namedatlas_load.nim
