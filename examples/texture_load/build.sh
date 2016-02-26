2f430332b5f2480af55b77c7721ae56b2e584f34
asdf <tehasdf@gmail.com>
2016-02-26 16:03:36 +0100

    atlas example

--- examples/texture_load/build.sh
+++ examples/texture_load/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p bin
-nim c $@ --nimcache=$(pwd)/.nimcache --debugger:native --out:bin/texture_load -p=$(pwd)/../../ texture_load.nim
+nim c $@ --nimcache=$(pwd)/.nimcache --debugger:native --out:bin/namedatlas_load -p=$(pwd)/../../ namedatlas_load.nim
